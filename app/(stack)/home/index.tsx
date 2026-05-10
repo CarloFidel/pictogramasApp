7d8ccd5eae3ee55a7f314df321d8b1bee1269d48
CarloFidel <carloftaboada@gmail.com>
2026-05-10 19:59:54 +0200

    feat:created navegationBar component

--- app/(stack)/home/index.tsx
+++ app/(stack)/home/index.tsx
@@ -1,21 +1,49 @@
+import NavegationBar from "@/comoponents/NavegationBar";
 import React from "react";
 import { Pressable, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import Feather from "@expo/vector-icons/Feather";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+
+let renderBurronsFlag = true;
 
 export default function Home() {
   return (
     <SafeAreaView className="flex-1 bg-primary">
       <StatusBar barStyle="light-content" />
+
       <View className=" bg-primary h-screen flex items-center">
-        <View className="bg-primary-600 px-8 py-2 mt-10 mb-5 rounded-3xl">
+        <View className="flex-row gap-8 bg-primary-600 px-8 py-4 mt-10 mb-5 rounded-3xl shadow-md shadow-primary-600">
           <Pressable>
-            <Text className="text-white text-2xl">+</Text>
+            <Feather name="plus" size={24} color="white" />
           </Pressable>
+          {renderBurronsFlag && (
+            <>
+              <Pressable>
+                <SimpleLineIcons
+                  name="pencil"
+                  size={18}
+                  color="white"
+                  className="mt-1"
+                />
+              </Pressable>
+              <Pressable>
+                <Feather name="play" size={20} color="white" className="mt-1" />
+              </Pressable>
+            </>
+          )}
         </View>
-        <Text className="text-white text-md font-hank-regular w-40 text-center">
-          Empieza añadiendo un pictograma
-        </Text>
-        <View className="w-8 h-screen bg-primary-400 rounded-lg mt-5"></View>
+        {!renderBurronsFlag && (
+          <Text className="text-white text-md font-hank-regular w-40 text-center">
+            Empieza añadiendo un pictograma
+          </Text>
+        )}
+
+        <View className="w-8 h-full bg-primary-400 rounded-lg mt-5"></View>
+        <View className="fixed bottom-1/3 z-10 shadow-md shadow-primary-700">
+          <NavegationBar></NavegationBar>
+        </View>
       </View>
     </SafeAreaView>
   );
