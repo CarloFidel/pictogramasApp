5ea2fe4b5e21abbc81bc981463010fc6f5a6ac71
CarloFidel <carloftaboada@gmail.com>
2026-06-26 10:59:12 +0200

    fix:fixed issue in auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,6 @@
-import { SessioExpiredContext } from "@/modules/auth/context/session-expired-provider/session-expired.context";
 import { Redirect } from "expo-router";
-import { use } from "react";
 
 const AppPicto = () => {
-  const session = use(SessioExpiredContext);
-
-  console.log(session?.sessionExpired);
-
   return (
     <>
       <Redirect href="/profile" />
